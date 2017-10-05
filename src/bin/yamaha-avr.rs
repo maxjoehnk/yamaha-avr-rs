609c187fd6a17170ebcba0fff055154e5b978b8d
Max Jöhnk <maxjoehnk@gmail.com>
2017-10-06 00:19:03 +0200

    improve power and mute commands and prepare for value fetching

--- src/bin/yamaha-avr.rs
+++ src/bin/yamaha-avr.rs
@@ -10,8 +10,8 @@ fn main() {
         (author: "Max Jöhnk <maxjoehnk@gmail.com>")
         (@arg ip: --ip +takes_value "Set the AVR Ip")
         (@subcommand power =>
-            (about: "Set Power")
-            (@arg value: +required "Set the Power state")
+            (about: "Get/Set Power")
+            (@arg value: "Get/Set the Power state")
         )
         (@subcommand mute =>
             (about: "Mute/Unmute")
@@ -28,12 +28,34 @@ fn main() {
     let ip: String = matches.value_of("ip").unwrap_or("192.168.2.102").to_owned();
     let mut avr = yamaha_avr::connect(ip);
     if let Some(matches) = matches.subcommand_matches("power") {
-        let value: bool = matches.value_of("value").unwrap().parse().unwrap();
-        avr.set_power(value).unwrap();
+        match matches.value_of("value") {
+            Some(value) => {
+                let parsed_value = parse_bool_state(value);
+                if parsed_value.is_some() {
+                    avr.set_power(parsed_value.unwrap());
+                }else {
+                    println!("Invalid value {}", value);
+                }
+            },
+            None => {
+                unimplemented!();
+            }
+        }
     }
     if let Some(matches) = matches.subcommand_matches("mute") {
-        let value: bool = matches.value_of("value").unwrap().parse().unwrap();
-        avr.set_mute(value).unwrap();
+        match matches.value_of("value") {
+            Some(value) => {
+                let parsed_value = parse_bool_state(value);
+                if parsed_value.is_some() {
+                    avr.set_mute(parsed_value.unwrap());
+                }else {
+                    println!("Invalid value {}", value);
+                }
+            },
+            None => {
+                unimplemented!();
+            }
+        }
     }
     if matches.subcommand_matches("inputs").is_some() {
         let inputs = avr.get_inputs().unwrap();
@@ -45,4 +67,13 @@ fn main() {
         let input = matches.value_of("input").unwrap().to_owned();
         avr.select_input(input, None).unwrap();
     }
+}
+
+fn parse_bool_state(input: &str) -> Option<bool> {
+    if input == "on" {
+        return Some(true);
+    }else if input == "off" {
+        return Some(false);
+    }
+    None
 }
